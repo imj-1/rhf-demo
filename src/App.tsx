32d4b31b5aa8d51c340789194bed161dcc2b9dce
Isidro <isidromolina.322@gmail.com>
2024-04-22 09:32:10 -0400

    form setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { YoutubeForm } from './components/YoutubeForm'
+import './App.css'
+
+function App() {
+  return (
+      <div>
+        <YoutubeForm />
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/YoutubeForm.tsx
@@ -0,0 +1,18 @@
+export const YoutubeForm = () => {
+  return (
+    <div>
+        <form>
+            <label htmlFor='username'>Username</label>
+            <input type="text" id="username"  name="username" />
+
+            <label htmlFor='email'>E-mail</label>
+            <input type="text" id="email"  name="email" />
+
+            <label htmlFor='channel'>Channel</label>
+            <input type="text" id="channel"  name="channel" />
+
+            <button>Submit</button>
+        </form>
+    </div>
+  )
+}
