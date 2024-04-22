335599cb67f6b95f8a102ac8ffecf1d8990263ab
imj-1 <isidromolina.322@gmail.com>
2024-04-22 10:54:16 -0400

    add register method to username input

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,14 +1,22 @@
 import { useForm } from "react-hook-form";
 
 export const YoutubeForm = () => {
-
   const form = useForm();
-  
+  const { register } = form;
+  const { name, ref, onChange, onBlur } = register("username");
+
   return (
     <div>
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input
+          type="text"
+          id="username"
+          name={name}
+          ref={ref}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
 
         <label htmlFor="email">E-mail</label>
         <input type="text" id="email" name="email" />
