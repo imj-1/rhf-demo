0fd5a94fead4e5dec357a74de5618e90dcb75397
imj-1 <isidromolina.322@gmail.com>
2024-04-22 10:47:41 -0400

    useForm hook

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,18 +1,23 @@
+import { useForm } from "react-hook-form";
+
 export const YoutubeForm = () => {
+
+  const form = useForm();
+  
   return (
     <div>
-        <form>
-            <label htmlFor='username'>Username</label>
-            <input type="text" id="username"  name="username" />
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" />
 
-            <label htmlFor='email'>E-mail</label>
-            <input type="text" id="email"  name="email" />
+        <label htmlFor="email">E-mail</label>
+        <input type="text" id="email" name="email" />
 
-            <label htmlFor='channel'>Channel</label>
-            <input type="text" id="channel"  name="channel" />
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" name="channel" />
 
-            <button>Submit</button>
-        </form>
+        <button>Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
