ab5db2ea964ecebb3a3e58a9db3f8447cf489be0
imj-1 <isidromolina.322@gmail.com>
2024-04-22 10:59:44 -0400

    simplify code by spreading register

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -3,26 +3,18 @@ import { useForm } from "react-hook-form";
 export const YoutubeForm = () => {
   const form = useForm();
   const { register } = form;
-  const { name, ref, onChange, onBlur } = register("username");
 
   return (
     <div>
       <form>
         <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          name={name}
-          ref={ref}
-          onChange={onChange}
-          onBlur={onBlur}
-        />
+        <input type="text" id="username" {...register("username")} />
 
         <label htmlFor="email">E-mail</label>
-        <input type="text" id="email" name="email" />
+        <input type="text" id="email" {...register("email")} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register("channel")} />
 
         <button>Submit</button>
       </form>
